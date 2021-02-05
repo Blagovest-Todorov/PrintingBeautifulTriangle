f8c822cae58c2be7e73b8d0e55fc7fa3fc5d8c36
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-05 09:44:05 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace TestTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumb = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = startNumb; i <= endNum; i++)
+
+            {
+                PrintNumbers(startNumb, i);
+            }
+
+            for (int i = endNum - 1; i >= startNumb; i--)
+            {
+                PrintNumbers(startNumb, i);
+            }
+        }
+
+        static void PrintNumbers(int start, int limit)
+        {
+            for (int i = start; i <= limit; i++)
+            {
+                Console.Write($"{i} ");
+
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
